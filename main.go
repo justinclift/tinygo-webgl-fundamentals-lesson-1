e7363063b3f5b62c57ad9d3454c0095130ea6b55
Justin Clift <justin@postgresql.org>
2019-07-24 23:11:32 +1000

    Convert shader source to use tabs, for consistency

--- main.go
+++ main.go
@@ -38,27 +38,26 @@ var (
 	// Vertex shader source code
 	vertCode = `
 	// an attribute will receive data from a buffer
-    attribute vec4 a_position;
-    
-    // all shaders have a main function
-    void main() {
-    
-      // gl_Position is a special variable a vertex shader
-      // is responsible for setting
-      gl_Position = a_position;
-    }`
+	attribute vec4 a_position;
+
+	// all shaders have a main function
+	void main() {
+		// gl_Position is a special variable a vertex shader
+		// is responsible for setting
+		gl_Position = a_position;
+	}`
 
 	// Fragment shader source code
 	fragCode = `
 	// fragment shaders don't have a default precision so we need
-    // to pick one. mediump is a good default. It means "medium precision"
-    precision mediump float;
-     
-    void main() {
-      // gl_FragColor is a special variable a fragment shader
-      // is responsible for setting
-      gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
-    }`
+	// to pick one. mediump is a good default. It means "medium precision"
+	precision mediump float;
+
+	void main() {
+		// gl_FragColor is a special variable a fragment shader
+		// is responsible for setting
+		gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+	}`
 )
 
 func main() {
